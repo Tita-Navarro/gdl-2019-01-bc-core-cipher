030f0501606c8a5013fa3cec292fb95b215bd780
Tita Navarro <45797993+Tita-Navarro@users.noreply.github.com>
2019-02-12 12:39:24 -0600

    commit para hacking edition

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -20,9 +20,9 @@ describe('cipher', () => {
     // Si decides implementar soporte para caracteres no alfabéticos descomenta
     // el test a continuación.
     //
-    // it('debería retornar " !@" para " !@"', () => {
-    //   assert.equal(cipher.encode(33, ' !@'),' !@');
-    // });
+    it('debería retornar " !@" para " !@"', () => {
+       assert.equal(cipher.encode(33, ' !@'),' !@');
+     });
 
     //
     // Hacker edition
@@ -50,12 +50,12 @@ describe('cipher', () => {
     // Si decides agregar soporte para minúsculas descomenta el test a
     // continuación.
     //
-    // it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
-    //   assert.equal(
-    //     cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg'),
-    //     'abcdefghijklmnopqrstuvwxyz'
-    //   );
-    // });
+     it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
+       assert.equal(
+         cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg'),
+         'abcdefghijklmnopqrstuvwxyz'
+       );
+     });
 
     //
     // Hacker edition
