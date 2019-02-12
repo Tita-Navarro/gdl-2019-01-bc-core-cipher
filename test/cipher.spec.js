6a183d32b102f001ebcb502d2f81e6de20d16285
Tita Navarro <45797993+Tita-Navarro@users.noreply.github.com>
2019-02-12 12:51:44 -0600

    commit test corregido

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -20,8 +20,10 @@ describe('cipher', () => {
     // Si decides implementar soporte para caracteres no alfabéticos descomenta
     // el test a continuación.
     //
-    it('debería retornar " !@" para " !@"', () => {
-       assert.equal(cipher.encode(33, ' !@'),' !@');
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () => {
+      assert.equal(
+        cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz'),
+        'hijklmnopqrstuvwxyzabcdefg');
      });
 
     //
